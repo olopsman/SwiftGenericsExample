317ef035c944b4a2fa72b5ecb5a2c187084a401b
Paulo Orquillo <olopsman@users.noreply.github.com>
2022-06-22 21:58:51 +1200

    Initial Commit

---
+++ SwiftGenericsExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftGenericsExample
+//
+//  Created by Paulo Orquillo on 22/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftGenericsExample/SwiftGenericsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenericsExampleApp.swift
+//  SwiftGenericsExample
+//
+//  Created by Paulo Orquillo on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenericsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
